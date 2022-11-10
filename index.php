fa3feee5ab0d39c1ea092e6ccaf2fa59d900b77e
Wilmer <95238585+wilmerthekiler21@users.noreply.github.com>
2022-11-09 18:23:48 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+     <header>
+        <div class="logo">
+          <img src="./img/Imagen de WhatsApp 2022-10-10 a las 19.49.04.jpg" alt="">
+          <h2>lll</h2>
+        </div>
+        <nav>
+            <a href="">Usuados y Antiguos</a>
+            <a href="">Pruebas de lecciones</a>
+            <a href="">Ofertas</a>
+            <a href="">Consejos</a>
+        </nav>
+     </header>
+    <form action="">
+      <div>
+        <div>
+          <img src="./img/electric-guitar-11530937008gqwe1aygab.png" alt="">
+          <h2>Precio Del profucto</h2>
+          <a href="">15000 MX</a>
+          <a href="">500 USA</a>
+          <h2>Producto antiguio</h2>
+        </div>
+        <div>
+          <img src="./img/373-3738830_inicio-guitarras-elctricas-guitarra-elctrica-orange-o-bass.png" alt="">
+          <h2>Precio Del profucto</h2>
+          <a href="">15000 MX</a>
+          <a href="">500 USA</a>
+          <h2>Producto antiguio</h2>
+        </div>
+        <div>
+          <img src="" alt="">
+          <a href=""></a>
+          <h2></h2>
+        </div>
+        <div>
+          <img src="" alt="">
+          <a href=""></a>
+          <h2></h2>
+        </div>
+      </div>
+    </form>
+</body>
+</html>
